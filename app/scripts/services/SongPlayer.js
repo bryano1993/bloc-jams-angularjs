d46b240d6c5bff0b3f1cbe47bc3e98157b1a3dbe
Bryan <bryano1993@gmail.com>
2018-01-06 21:29:45 -0800

    assignment-7-services2: complete

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,14 +1,20 @@
 (function() {
   function SongPlayer() {
+    /*
+    * @desc empty SongPlayer object
+    * @type {Object} Song Player
+    */
     var SongPlayer = {};
-
+    /*
+    * @desc selected song
+    * @type {Object} currentSong
+    */
     var currentSong = null;
-  /*
- * @desc Buzz object audio file
- * @type {Object}
- */
+   /*
+    * @desc Buzz object audio file
+    * @type {Object}
+    */
     var currentBuzzObject = null;
-
     /*
      * @function setSong
      * @desc Stops currently playing song and loads new audio file as currentBuzzObject
@@ -27,7 +33,10 @@
 
       currentSong = song;
     }
-
+    /*
+     * @desc method to play a selected song
+     * @param {Object} song
+     */
     SongPlayer.play = function(song) {
       if (currentSong !== song){
         setSong(song);
@@ -40,12 +49,24 @@
         }
     };
 
+    /*
+     * @desc method that pauses selected songs
+     * @param {Object} song
+     */
     SongPlayer.pause = function(song) {
       currentBuzzObject.pause();
       song.playing = false;
     }
     return SongPlayer;
   }
+   /*
+   * @desc SongPlayer object
+   * @function playSong
+   */
+    var playSong = function (){
+      currentBuzzObject.play();
+      song.playing = true;
+    }
 
   angular
     .module('blocJams')
