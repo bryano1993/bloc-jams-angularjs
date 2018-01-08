4807fe1c3109ce69946ab9508d882fa30d955f20
Bryan <bryano1993@gmail.com>
2018-01-08 13:53:33 -0800

    checkpoint-7-services2: edit

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -3,27 +3,28 @@
     var SongPlayer = {};
 
     var currentSong = null;
-  /*
- * @desc Buzz object audio file
- * @type {Object}
- */
+
+    /**
+   * @desc Buzz object audio file
+   * @type {Object}
+   */
     var currentBuzzObject = null;
 
-    /*
+     /**
      * @function setSong
      * @desc Stops currently playing song and loads new audio file as currentBuzzObject
      * @param {Object} song
      */
     var setSong = function(song) {
-      if (currentBuzzObject) {
-        currentBuzzObject.stop();
-        currentSong.playing = null;
+    if (currentBuzzObject) {
+      currentBuzzObject.stop();
+      currentSong.playing = null;
       }
 
-      currentBuzzObject = new buzz.sound(song.audioUrl, {
-        formats: ['mp3'],
-        preload: true
-      });
+    currentBuzzObject = new buzz.sound(song.audioUrl, {
+      formats: ['mp3'],
+      preload: true
+    });
 
       currentSong = song;
     }
@@ -43,7 +44,7 @@
     SongPlayer.pause = function(song) {
       currentBuzzObject.pause();
       song.playing = false;
-    }
+    };
     return SongPlayer;
   }
 
